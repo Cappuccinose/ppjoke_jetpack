8d50c51495de8e093d09365e934ad2ddc428032f
Cappuccino <arons.hui@gmail.com>
2020-04-04 23:03:25 +0800

    home->viewmodel

--- app/src/main/java/com/mooc/ppjock/ui/home/HomeViewModel.java
+++ app/src/main/java/com/mooc/ppjock/ui/home/HomeViewModel.java
@@ -10,6 +10,9 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
+
+
+
         mText.setValue("This is home fragment");
     }
 
