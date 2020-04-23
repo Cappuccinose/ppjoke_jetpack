08b789d8661460fade35c9214babac0c36f6d0ed
Cappuccino <arons.hui@gmail.com>
2020-04-23 17:11:06 +0800

    视频播放接口

--- app/src/main/java/com/mooc/ppjock/exoplayer/IPlayTarget.java
+++ app/src/main/java/com/mooc/ppjock/exoplayer/IPlayTarget.java
@@ -1,4 +1,17 @@
 package com.mooc.ppjock.exoplayer;
 
+import android.view.ViewGroup;
+
 public interface IPlayTarget {
+
+    ViewGroup getOwner();
+
+    //活跃状态视频可播放
+    void onActive();
+
+    //非活跃状态，暂停
+    void inActive();
+
+    boolean isPlaying();
+
 }
