a6ba82cf46a962e043e3c73879266d20e6283196
Cappuccino <arons.hui@gmail.com>
2020-05-01 23:13:04 +0800

    处理点击事件新增布局

--- app/src/main/java/com/mooc/ppjock/ui/detail/FeedDetailActivity.java
+++ app/src/main/java/com/mooc/ppjock/ui/detail/FeedDetailActivity.java
@@ -1,8 +1,21 @@
 package com.mooc.ppjock.ui.detail;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FeedDetailActivity extends AppCompatActivity {
     private static final String KEY_FEED = "key_feed";
     public static final String KEY_CATEGORY = "key_category";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
