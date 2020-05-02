cc387e34568b7281cc0a7569f6e6b027a665d057
Cappuccino <arons.hui@gmail.com>
2020-05-02 21:35:33 +0800

    视图

---
+++ app/src/main/java/com/mooc/ppjock/ui/detail/VideoViewHandler.java
@@ -0,0 +1,13 @@
+package com.mooc.ppjock.ui.detail;
+
+import android.content.Context;
+
+public class VideoViewHandler extends ViewHandler {
+
+    private final Context mContext;
+
+    public VideoViewHandler(Context context) {
+
+        this.mContext = context;
+    }
+}
