90dc7a87e43964c10b7fe0aeb338bcd1540e4dd9
Cappuccino <arons.hui@gmail.com>
2020-05-03 22:28:13 +0800

    数据绑定

--- app/src/main/java/com/mooc/ppjock/ui/detail/ViewHandler.java
+++ app/src/main/java/com/mooc/ppjock/ui/detail/ViewHandler.java
@@ -1,13 +1,16 @@
 package com.mooc.ppjock.ui.detail;
 
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 public class ViewHandler {
 
     private final FragmentActivity mActivity;
+    private final FeedDetailViewModel mDetailViewModel;
 
     public ViewHandler(FragmentActivity activity) {
         mActivity = activity;
+        mDetailViewModel = ViewModelProviders.of(activity).get(FeedDetailViewModel.class);
     }
 }
