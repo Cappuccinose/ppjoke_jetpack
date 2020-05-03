ed41591bbc48d99828c5cafe84f9edbb9a5d1859
Cappuccino <arons.hui@gmail.com>
2020-05-03 22:27:55 +0800

    数据绑定

---
+++ app/src/main/java/com/mooc/ppjock/ui/detail/FeedDetailViewModel.java
@@ -0,0 +1,13 @@
+package com.mooc.ppjock.ui.detail;
+
+import androidx.paging.DataSource;
+
+import com.mooc.ppjock.AbsViewModel;
+import com.mooc.ppjock.model.Comment;
+
+public class FeedDetailViewModel extends AbsViewModel<Comment> {
+    @Override
+    public DataSource createDataSource() {
+        return null;
+    }
+}
