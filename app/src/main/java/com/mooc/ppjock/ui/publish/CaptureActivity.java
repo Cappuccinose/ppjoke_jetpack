91605ceb2b547e7aa73d5b6e13a305466151b5b4
Cappuccino <arons.hui@gmail.com>
2020-05-18 22:17:43 +0800

    CaptureActivity

--- app/src/main/java/com/mooc/ppjock/ui/publish/CaptureActivity.java
+++ app/src/main/java/com/mooc/ppjock/ui/publish/CaptureActivity.java
@@ -1,8 +1,16 @@
 package com.mooc.ppjock.ui.publish;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class CaptureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
 
